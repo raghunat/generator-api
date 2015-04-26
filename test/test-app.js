3e407e9be15ae08868b9ddda500259a6839b4df5
Professor Raghunath <raghunat@fredonia.edu>
2015-04-26 07:26:32 -0400

    Updating travis build

--- test/test-app.js
+++ test/test-app.js
@@ -3,13 +3,16 @@
 var path = require('path');
 var assert = require('yeoman-generator').assert;
 var helpers = require('yeoman-generator').test;
-var os = require('os');
 
 describe('api:app', function () {
   before(function (done) {
     helpers.run(path.join(__dirname, '../app'))
-      .withOptions({ skipInstall: true })
-      .withPrompts({ someOption: true })
+      .withOptions({
+        skipInstall: true
+      })
+      .withPrompts({
+        someOption: true
+      })
       .on('end', done);
   });
 
